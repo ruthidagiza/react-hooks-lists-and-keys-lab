559afaf0c45478df68d2b9ffde9f823e18e85791
IDAGIZA <ruthidagiza@gmail.com>
2022-07-19 00:41:58 +0300

    commit tree a6087e1c27f6a7d3a0c6e9a2a96ea3a2f698ddd1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const myLink=links.map((link)=>
+  <a href={ '#' + link} key={link}>{link}</a>
+  )
+
+
+  return <nav>{/* display an <a> tag for each link here */}
+  {myLink}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const tech=technologies.map((technology)=>
+ <span key={technology}>{technology}</span>
+  )
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
+      <div className="technologies">{tech}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
